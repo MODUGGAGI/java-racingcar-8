05e10dde062e3a0dbdad2275dc331a3ad35188d8
MODUGGAGI <kiroro0814@naver.com>
2025-10-24 14:18:51 +0900

    feat(domain): Race 도메인 클래스 구현
    아래의 기능을 구현했습니다.
    1. 매 경주 라운드 진행
    2. 우승자 결정

---
+++ src/main/java/racingcar/domain/Race.java
@@ -0,0 +1,60 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private ArrayList<Car> carList;
+    private int racingRounds;
+
+    public Race(ArrayList<Car> carList, int racingRounds) {
+        this.carList = carList;
+        this.racingRounds = racingRounds;
+    }
+
+    public String startRacing() {
+
+        StringBuilder raceLog = new StringBuilder();
+
+        for (int i = 0; i < racingRounds; i++) {
+            playEachRound(raceLog);
+        }
+
+        return raceLog.toString();
+    }
+
+    public List<String> findWinners() {
+
+        int maxPosition = findMaxPosition();
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : carList) {
+            addIfWinner(car, maxPosition, winners);
+        }
+
+        return winners;
+    }
+
+    private void playEachRound(StringBuilder raceLog) {
+        for (Car car : carList) {
+            car.move();
+            raceLog.append(car.drawPosition()).append("\n");
+        }
+        raceLog.append("\n");
+    }
+
+    private int findMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : carList) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
+
+    private void addIfWinner(Car car, int maxPosition, List<String> winners) {
+        if (maxPosition == car.getPosition()) {
+            winners.add(car.getName());
+        }
+    }
+}
