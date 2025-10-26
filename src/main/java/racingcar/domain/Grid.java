143667777fd6b6181c33cb5e936181480758e0c8
MODUGGAGI <kiroro0814@naver.com>
2025-10-26 22:56:05 +0900

    refactor(domain): carList를 일급 콜렉션 Grid로 포장
    기존에 존재하던 Race 내부 ArrayList<Car> 필드를 일급 콜렉션으로 포장해서 Race 클래스는 경주의 전체 실행을 담당하고, Grid는 참가한 차량들의 관리를 하게끔 책임을 분리했습니다.

---
+++ src/main/java/racingcar/domain/Grid.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grid {
+
+    private final List<Car> carList;
+
+    public Grid(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public String race() {
+        StringBuilder result = new StringBuilder();
+
+        for (Car car : carList) {
+            car.move();
+            result.append(car.drawPosition()).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    public List<String> findWinnerNames() {
+        int maxPosition = findMaxPosition();
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : carList) {
+            car.ifPositionThenGetName(maxPosition)
+                    .ifPresent(winners::add);
+        }
+
+        return winners;
+    }
+
+    private int findMaxPosition() {
+        int maxPosition = 0;
+
+        for (Car car : carList) {
+            maxPosition = car.getMaxPosition(maxPosition);
+        }
+
+        return maxPosition;
+    }
+}
--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -5,50 +5,25 @@ import java.util.List;
 
 public class Race {
 
-    private ArrayList<Car> carList;
-    private int racingRounds;
+    private final Grid grid;
+    private final int racingRounds;
 
     public Race(ArrayList<Car> carList, int racingRounds) {
-        this.carList = carList;
+        this.grid = new Grid(carList);
         this.racingRounds = racingRounds;
     }
 
     public String startRacing() {
-
         StringBuilder raceLog = new StringBuilder();
 
         for (int i = 0; i < racingRounds; i++) {
-            playEachRound(raceLog);
+            raceLog.append(grid.race()).append("\n");
         }
 
         return raceLog.toString();
     }
 
     public List<String> findWinners() {
-
-        int maxPosition = findMaxPosition();
-
-        List<String> winners = new ArrayList<>();
-        for (Car car : carList) {
-            car.ifPositionThenGetName(maxPosition).ifPresent(winners::add);
-        }
-
-        return winners;
-    }
-
-    private void playEachRound(StringBuilder raceLog) {
-        for (Car car : carList) {
-            car.move();
-            raceLog.append(car.drawPosition()).append("\n");
-        }
-        raceLog.append("\n");
-    }
-
-    private int findMaxPosition() {
-        int maxPosition = 0;
-        for (Car car : carList) {
-            maxPosition = car.getMaxPosition(maxPosition);
-        }
-        return maxPosition;
+        return grid.findWinnerNames();
     }
 }
